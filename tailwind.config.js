4dbf7d15c1de50edae2e9d3426040c2f84f640f8
Mateus Musial <mateus.musial@gmail.com>
2022-05-17 23:34:33 -0300

    Header and main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "wall-theme": "url('../public/background.jpg')",
+      }),
+      backgroundSize: {
+        "100%": "100%",
+        "195%": "195%",
+      },
+      backgroundPosition: {
+        banner: "46% 4%",
+        "banner-mobile": "top 25% center",
+      },
+    },
   },
   plugins: [],
 };
